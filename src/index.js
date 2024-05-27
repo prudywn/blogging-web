75cd587cdef04064af7cadcd8f65d9e6eb9cff63
itsPrudy <dencecira463@gmail.com>
2024-05-27 19:21:37 +0300

    Made some changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import { render } from '@testing-library/react'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { DarkModeProvider } from './components/DarkModeContext';
-  
 
-ReactDOM.createRoot(document.getElementById('root'), render(
-  <>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <DarkModeProvider>
-        <App />
-      </DarkModeProvider>,
+      <App />
+    </DarkModeProvider>
   </React.StrictMode>
-  </>
-  ))
+);
