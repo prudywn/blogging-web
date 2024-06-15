d3a51d930260f39598e0861a15ec5a16c4690d04
itsPrudy <dencecira463@gmail.com>
2024-06-15 14:26:12 +0300

    Fixed some errors

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,12 +1,8 @@
-
+// Settings.js
 import './settings.css';
 import React, { useState, useEffect } from 'react';
-import { useUser } from '../../UserContext'
-
-import React, { useState } from 'react';
+import { useUser } from '../../UserContext';
 import axios from 'axios';
-import "./settings.css"
-
 
 export default function Settings() {
   const { userData, updateUser } = useUser();
@@ -15,15 +11,14 @@ export default function Settings() {
     email: userData.email,
     password: '',
     profilePic: null,
-
     profilePicPreview: userData.profilePic
   });
 
   useEffect(() => {
     if (userData.profilePic){
-      setFormData((prevData) => ({...prevData, profilePicPreview: userData.profilePic}))
+      setFormData((prevData) => ({...prevData, profilePicPreview: userData.profilePic}));
     }
-  }, [userData.profilePic])
+  }, [userData.profilePic]);
 
   const [likedImages, setLikedImages] = useState([]);
   const [favoriteImages, setFavoriteImages] = useState([]);
@@ -57,81 +52,14 @@ export default function Settings() {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     updateUser({
       username: formData.username,
       email: formData.email,
       profilePic: formData.profilePicPreview,
     });
-    console.log('Updated user data: ', formData);
-  };
 
-  return (
-    <div>
-      <div>
-        <img src='https://blog.depositphotos.com/wp-content/uploads/2017/07/Soothing-nature-backgrounds-2.jpg.webp' alt="" style={{ width: '70%', margin: '10px', padding: '10px' }} />
-      </div>
-      <div className="settings">
-        <div className="settingsWrapper">
-          <div className="settingsTitle">
-            <span className="settingsUpdateTitle">Update Your Account</span>
-            <span className="settingsDeleteTitle">Delete Account</span>
-          </div>
-          <form className="settingsForm" onSubmit={handleSubmit}>
-            <label>Profile Picture
-              <input type="file" name="profilePic" accept='image/*' onChange={handleChange} />
-            </label>
-            {formData.profilePicPreview && (
-              <div className="settingsProfile">
-                <img src={(formData.profilePicPreview)} alt="profilePic" style={{ width: '100px', height: '100px' }} />
-                <label htmlFor="fileInput">
-                  <i className=" settingsProfilePicIcon fas fa-user-circle"></i>
-                </label>
-                <input type="file" id='fileInput' style={{ display: 'none' }} />
-              </div>
-            )}
-            <label htmlFor="userName">Username</label>
-            <input value={formData.username} name='username' type="text" placeholder='Your name...' onChange={handleChange} />
-            <label htmlFor="email">Email</label>
-            <input value={formData.email} name='email' type="email" placeholder='Your email...' onChange={handleChange} />
-            <label htmlFor="passwd">Password</label>
-            <input value={formData.password} name='password' type="password" onChange={handleChange} />
-            {/* Display liked and favorite images */}
-            <div>
-              <h2>Liked Images</h2>
-              <ul>
-                {likedImages.map((image, index) => (
-                  <li key={index}>
-                    <img src={image} alt={`Liked Image ${index + 1}`} style={{ width: '100px', height: '100px', marginRight: '10px' }} />
-                  </li>
-                ))}
-              </ul>
-              <h2>Favorite Images</h2>
-              <ul>
-                {favoriteImages.map((image, index) => (
-                  <li key={index}>
-                    <img src={image} alt={`Favorite Image ${index + 1}`} style={{ width: '100px', height: '100px', marginRight: '10px' }} />
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <button className='settingsSubmit'>Update</button>
-          </form>
-        </div>
-
-  });
-
-  const handleChange = (e) => {
-    const { name, value, files } = e.target;
-    setFormData({
-      ...formData,
-      [name]: files ? files[0] : value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
     const token = localStorage.getItem('token');
     const config = {
       headers: {
@@ -156,26 +84,56 @@ export default function Settings() {
   };
 
   return (
-    <div className="settings">
-      <div className="settingsWrapper">
-        <div className="settingsTitle">
-          <span className="settingsUpdateTitle">Update Your Account</span>
+    <div>
+      <div>
+        <img src='https://blog.depositphotos.com/wp-content/uploads/2017/07/Soothing-nature-backgrounds-2.jpg.webp' alt="" style={{ width: '70%', margin: '10px', padding: '10px' }} />
+      </div>
+      <div className="settings">
+        <div className="settingsWrapper">
+          <div className="settingsTitle">
+            <span className="settingsUpdateTitle">Update Your Account</span>
+            <span className="settingsDeleteTitle">Delete Account</span>
+          </div>
+          <form className="settingsForm" onSubmit={handleSubmit}>
+            <label>Profile Picture
+              <input type="file" name="profilePic" accept='image/*' onChange={handleChange} />
+            </label>
+            {formData.profilePicPreview && (
+              <div className="settingsProfile">
+                <img src={formData.profilePicPreview} alt="profilePic" style={{ width: '100px', height: '100px' }} />
+                <label htmlFor="fileInput">
+                  <i className=" settingsProfilePicIcon fas fa-user-circle"></i>
+                </label>
+                <input type="file" id='fileInput' style={{ display: 'none' }} />
+              </div>
+            )}
+            <label htmlFor="username">Username</label>
+            <input value={formData.username} name='username' type="text" placeholder='Your name...' onChange={handleChange} />
+            <label htmlFor="email">Email</label>
+            <input value={formData.email} name='email' type="email" placeholder='Your email...' onChange={handleChange} />
+            <label htmlFor="password">Password</label>
+            <input value={formData.password} name='password' type="password" onChange={handleChange} />
+            <div>
+              <h2>Liked Images</h2>
+              <ul>
+                {likedImages.map((image, index) => (
+                  <li key={index}>
+                    <img src={image} alt={`Liked Image ${index + 1}`} style={{ width: '100px', height: '100px', marginRight: '10px' }} />
+                  </li>
+                ))}
+              </ul>
+              <h2>Favorite Images</h2>
+              <ul>
+                {favoriteImages.map((image, index) => (
+                  <li key={index}>
+                    <img src={image} alt={`Favorite Image ${index + 1}`} style={{ width: '100px', height: '100px', marginRight: '10px' }} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <button className='settingsSubmit'>Update</button>
+          </form>
         </div>
-        <form className="settingsForm" onSubmit={handleSubmit}>
-          <label htmlFor="username">Username</label>
-          <input value={formData.username} name="username" type="text" placeholder="Your username..." onChange={handleChange} />
-          <label htmlFor="email">Email</label>
-          <input value={formData.email} name="email" type="email" placeholder="Your email..." onChange={handleChange} />
-          <label htmlFor="password">Password</label>
-          <input value={formData.password} name="password" type="password" placeholder="Your password..." onChange={handleChange} />
-          <label htmlFor="profilePic">Profile Picture</label>
-          <input name="profilePic" type="file" accept="image/*" onChange={handleChange} />
-          {formData.profilePic && (
-            <img src={URL.createObjectURL(formData.profilePic)} alt="Profile" style={{ width: '100px', height: '100px' }} />
-          )}
-          <button className="settingsSubmit" type="submit">Update</button>
-        </form>
-
       </div>
     </div>
   );
